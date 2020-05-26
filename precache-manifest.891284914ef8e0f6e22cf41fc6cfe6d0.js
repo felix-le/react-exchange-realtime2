308dbaf6104036a82486cf3824003b661edd935e
Viet Anh Le <mc.vietanh@gmail.com>
2020-05-26 18:20:07 +0700

    Updates

---
+++ precache-manifest.891284914ef8e0f6e22cf41fc6cfe6d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc9aed649f71073eaaca7e03c23dfb2b",
+    "url": "/react-exchange-realtime2/index.html"
+  },
+  {
+    "revision": "ad097842f1e74d48b4a7",
+    "url": "/react-exchange-realtime2/static/css/main.e2a38ef4.chunk.css"
+  },
+  {
+    "revision": "f8d3864f36c098000602",
+    "url": "/react-exchange-realtime2/static/js/2.7e9a086f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-exchange-realtime2/static/js/2.7e9a086f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad097842f1e74d48b4a7",
+    "url": "/react-exchange-realtime2/static/js/main.668ba379.chunk.js"
+  },
+  {
+    "revision": "ec5fd4cf689b068e7bc2",
+    "url": "/react-exchange-realtime2/static/js/runtime-main.915df9e3.js"
+  }
+]);
